a4c8d7bb9e45b0fa5b42799edfb214fd127b0b3f
Lucas Lando <lucaslando1@gmail.com>
2014-09-28 13:21:16 -0300

    Implementados los servicios para obtener, crear y editar categorias y subcategorias

--- desapp-groupD-backend/src/main/java/ar/edu/unq/desapp/grupoD/services/CategoryAndSubcategoryService.java
+++ desapp-groupD-backend/src/main/java/ar/edu/unq/desapp/grupoD/services/CategoryAndSubcategoryService.java
@@ -2,14 +2,17 @@ package ar.edu.unq.desapp.grupoD.services;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
 
 import ar.edu.unq.desapp.grupoD.model.category.Category;
+import ar.edu.unq.desapp.grupoD.model.category.SubCategory;
 import ar.edu.unq.desapp.grupoD.persistence.CategoryDao;
 
 /**
@@ -31,33 +34,30 @@ public class CategoryAndSubcategoryService {
     }
 	
 	
-//	@POST
-//    @Path("/category/{name}")
-//    @Consumes("application/json")
-//	@Produces({"application/xml","application/json"})
-//    public Response saveOrUpdateCategory(@PathParam("name") String name) {
-//		Category category = new Category(name);
-//		categoryDao.saveOrUpdateCategory(category);
-//        return Response.ok().build();
-//    }
-//	
-//	@POST
-//    @Path("/subcategory/{name}")
-//    @Consumes("application/json")
-//	@Produces({"application/xml","application/json"})
-//    public Response saveOrUpdateSubCategory(@PathParam("name") String name) {
-//		SubCategory subcategory = new SubCategory(name);
-//		categoryDao.saveOrUpdateSubCategory(subcategory);
-//        return Response.ok().build();
-//    }
-//
-//	@GET
-//    @Path("/subcategory")
-//    @Produces("application/json")
-//    public List<SubCategory> getAllSubCategories() {
-//        List<SubCategory> subcategoriesList = categoryDao.getAllSubCategories();
-//        return subcategoriesList;
-//    }
-//	
-//	
+	@GET
+    @Path("/add/{name}")
+    @Consumes("application/json")
+    public Response saveOrUpdateCategory(@PathParam("name") String name) {
+		Category category = new Category(name);
+		categoryDao.saveOrUpdateCategory(category);
+        return Response.ok().build();
+    }
+
+	
+	@GET
+    @Path("/subcategory/{name}")
+    @Consumes("application/json")
+    public Response saveOrUpdateSubCategory(@PathParam("name") String name) {
+		SubCategory subcategory = new SubCategory(name);
+		categoryDao.saveOrUpdateSubCategory(subcategory);
+        return Response.ok().build();
+    }
+
+	@GET
+    @Path("/subcategory")
+    @Produces("application/json")
+    public List<SubCategory> getAllSubCategories() {
+        List<SubCategory> subcategoriesList = categoryDao.getAllSubCategories();
+        return subcategoriesList;
+    }
 }
